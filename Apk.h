c2895186bb3f4c3559609ba3ed7812934c7f5f86
thanatos <weloxyz@gmail.com>
2023-11-09 14:55:32 +0800

    优化错误处理

--- Apk.h
+++ Apk.h
@@ -17,11 +17,10 @@ class Apk {
    private:
     std::unique_ptr<aapt::io::IFileCollection> collection_;
     std::unique_ptr<android::AssetManager> assetManager_;
-    std::unique_ptr<aapt::xml::XmlResource> manifest_;
 
    public:
-    Apk(std::unique_ptr<aapt::io::IFileCollection> collection, std::unique_ptr<android::AssetManager> assetManager, std::unique_ptr<aapt::xml::XmlResource> manifest)
-        : collection_(std::move(collection)), assetManager_(std::move(assetManager)), manifest_(std::move(manifest)){};
+    Apk(std::unique_ptr<aapt::io::IFileCollection> collection, std::unique_ptr<android::AssetManager> assetManager)
+        : collection_(std::move(collection)), assetManager_(std::move(assetManager)){};
     ~Apk() = default;
 
     android::AssetManager* GetAssetManager() const {
@@ -32,19 +31,25 @@ class Apk {
         return collection_.get();
     }
 
-    /// @brief zip格式加载apk、解析resources.arsc、AndroidManifest.xml
+    /// @brief zip格式加载apk、即使resources.arsc、AndroidManifest.xml不存在，也可以加载
     /// @param path apk路径
     /// @return 失败返回nullptr
     static std::unique_ptr<Apk> LoadApkFromPath(const std::string& path);
 
+    /// @brief 解析manifest
+    /// @return 失败返回nullptr, 没有resources.arsc和AndroidManifest.xml返回空字符串
     std::unique_ptr<std::string> GetManifest() const;
 
     /// @brief 获取resource.arsc中的字符串池
-    /// @return 如果字符串池为空则返回空list, 错误则返回nullptr
+    /// @return 失败返回nullptr, 没有resources.arsc或其中没有字符串,返回空字符串列表
     std::unique_ptr<std::list<std::string>> GetStrings() const;
 
+    /// @brief 解析所有dex的class和string
+    /// @return 永远不会返回nullptr, 没有dex返回空列表
     std::unique_ptr<std::pair<std::set<std::string>, std::set<std::string>>> ParseDexes() const;
 
+    /// @brief 执行所有的任务, 并返回json
+    /// @return 某个任务失败返回nullptr
     std::unique_ptr<nlohmann::json> DoAllTasks() const;
 
     // 删除字符串中的\r \n \t 空格
